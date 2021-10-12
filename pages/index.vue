286af20804122c1a66e75c2d009cac18f33c3111
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-10-12 03:43:06 +0200

    HTML improvements

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,140 @@
 <template>
-  <Tutorial/>
+  <div class="bg-white p-16">
+    <form class="space-y-8 divide-y divide-gray-200">
+      <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+        <div>
+          <div>
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+              Video format converter
+            </h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+              Convert videos from one format to another using this utility.
+            </p>
+          </div>
+
+          <div class="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+            <div
+              class="
+                sm:grid sm:grid-cols-3
+                sm:gap-4
+                sm:items-start
+                sm:border-t sm:border-gray-200
+                sm:pt-5
+              "
+            >
+              <label
+                for="video"
+                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+              >
+                Video
+              </label>
+              <div class="mt-1 sm:mt-0 sm:col-span-2">
+                <input
+                  type="file"
+                  name="video"
+                  id="video"
+                  class="
+                    flex-1
+                    block
+                    w-full
+                    focus:ring-indigo-500
+                    focus:border-indigo-500
+                    min-w-0
+                    rounded-none rounded-r-md
+                    sm:text-sm
+                    border-gray-300
+                  "
+                />
+              </div>
+            </div>
+
+            <div
+              class="
+                sm:grid sm:grid-cols-3
+                sm:gap-4
+                sm:items-start
+                sm:border-t sm:border-gray-200
+                sm:pt-5
+              "
+            >
+              <label
+                for="format"
+                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+              >
+                Format
+              </label>
+              <div class="mt-1 sm:mt-0 sm:col-span-2">
+                <select
+                  id="format"
+                  name="format"
+                  rows="3"
+                  class="
+                    max-w-lg
+                    shadow-sm
+                    block
+                    w-full
+                    focus:ring-indigo-500
+                    focus:border-indigo-500
+                    sm:text-sm
+                    border border-gray-300
+                    rounded-md
+                  "
+                ></select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-5">
+        <div class="flex justify-end">
+          <button
+            type="button"
+            class="
+              bg-white
+              py-2
+              px-4
+              border border-gray-300
+              rounded-md
+              shadow-sm
+              text-sm
+              font-medium
+              text-gray-700
+              hover:bg-gray-50
+              focus:outline-none
+              focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+            "
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            class="
+              ml-3
+              inline-flex
+              justify-center
+              py-2
+              px-4
+              border border-transparent
+              shadow-sm
+              text-sm
+              font-medium
+              rounded-md
+              text-white
+              bg-indigo-600
+              hover:bg-indigo-700
+              focus:outline-none
+              focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+            "
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
